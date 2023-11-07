56c8937395e85778073cc363fb70b62e735e67c8
Doctorixx <61980858+Windows-up@users.noreply.github.com>
2023-11-07 18:03:53 +0300

    New example

--- programms/Example/tests.py
+++
@@ -1,9 +0,0 @@
-from core.runner import Runner
-
-runner = Runner()
-
-input_data = [15, 13]
-
-out = runner.run_many(input_data)
-runner.save_tests(out, input_data)
-runner.build(indent=2)
---
+++ programms/Пример/build.py
@@ -0,0 +1,23 @@
+import random
+
+from core.runner import Runner
+
+runner = Runner()
+
+examples = {
+    "1\n5": "5",
+    "-5\n10": "-50",
+}
+
+input_data = []
+
+for i in range(20):
+    a = random.randint(-10 ** 5, 10 ** 5)
+    b = random.randint(-10 ** 5, 10 ** 5)
+    input_data.append(f"{a}\n{b}")
+
+out = runner.run_many(input_data)
+runner.save_tests(out, input_data)
+runner.save_examples(examples)
+runner.build(indent=2)
+print("[+] Done")
